e400e0eaaaefe724b3a015b292b59d58ce13ef1e
Sameh <sameh.benamor@esprit.tn>
2024-04-15 20:22:58 +0100

    Teaching github

--- soretrak/configuration.js
+++ soretrak/configuration.js
@@ -3,3 +3,4 @@ export const BACKEND_URL_User = 'http://' + Base + ':3000/user/';
 export const BACKEND_URL_ligne = 'http://' + Base + ':3000/ligne/';
 export const BACKEND_URL_reservation = 'http://' + Base + ':3000/reservation/';
 export const BACKEND_URL_bus = 'http://' + Base + ':3000/bus/';
+//cc noor
